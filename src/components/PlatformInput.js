a8de0b09b33ed8eaa048d5b4b943855f5c671f1f
Ella <michaella.latkovic@gmail.com>
2019-12-11 15:35:50 -0800

    added Platform and PlatformInput components

---
+++ src/components/PlatformInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class PlatformInput extends React.Component {
+
+  render() {
+      return (
+          <div>
+          Platform Input Form
+          </div>
+      )
+  }
+  
+}
+
+export default (PlatformInput)
---
+++ src/components/Platforms.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Platforms = (props) => {
+
+  return (
+    <div>
+    Platform List
+    </div>
+  )
+  
+}
+
+export default Platforms
