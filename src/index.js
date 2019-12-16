73a304dc1408108d293633a6d9056cd0338ec1cf
Ella <michaella.latkovic@gmail.com>
2019-12-15 20:19:24 -0800

    added BrowserRouter and wrapped <App> in Router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
+
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 
 import { Provider } from 'react-redux';
-
-import App from './App';
-
 import store from './store.js'
+import App from './App';
 
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
     document.getElementById('root')
   );
